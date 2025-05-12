15a4a75625b25be9cdeacbcca506fcdc8c7fb9d2
AbdulRahman04-1 <arahmankhanamk@gmail.com>
2025-05-12 00:26:20 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Add new routes here
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
